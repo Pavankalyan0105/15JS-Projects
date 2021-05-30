f2dfa9b7c6b2e610161c5340e520cce2e4bfb870
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-05-30 21:15:28 +0530

    completed

--- colorFlipper/script.js
+++ colorFlipper/script.js
@@ -1,5 +1,3 @@
-document.body.style.background = "";
-
 
 
 
@@ -7,14 +5,16 @@ const colors = ["red" , "green" , "purple" , "yellow"]
 var clickCount=0
 var n = colors.length;
 
-const btn   = document.querySelector("#button");
-const color = document.querySelector(".color-highlight");
+const btn   = document.querySelector(".btn");
+const color = document.querySelector(".color");
 
-color.innerHTML= "#24232A";
+
 
 
 btn.addEventListener("click",()=>{
     clickCount++;
-    color.innerHTML = colors[clickCount%n]
+    color.innerHTML = colors[clickCount%n];
     document.body.style.background = colors[clickCount%n];
+
+    // document.body.style.background = "green";
 })
