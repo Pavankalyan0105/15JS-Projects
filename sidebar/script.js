8ec053b4e086a88930b639f4f7ac5579e2b82962
pavankalyan0105 <pavankalyan0105@gmail.com>
2021-06-04 22:00:25 +0530

    completed sidebar

---
+++ sidebar/script.js
@@ -0,0 +1,7 @@
+
+const toggleBtn = document.querySelector(".sidebar-toggle");
+const sidebar   = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click",()=>{
+   sidebar.classList.toggle("show-sidebar");
+})
